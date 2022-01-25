ec94d149e5b4a64b29bc650753052f68aabf52fd
c200623 <c200623@ccg.ac.jp>
2022-01-25 04:34:48 -0800

    Security Controler 追加します

---
+++ src/main/java/jp/ac/ccmc/_2j/kimatsu2021/Security.java
@@ -0,0 +1,50 @@
+package jp.ac.ccmc._2j.kimatsu2021;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter{
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/css/**");
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+        .authorizeRequests()
+        .antMatchers("/", "/index").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+        .logout()
+        .permitAll();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        String password = passwordEncoder().encode("c200623");
+        auth
+        .inMemoryAuthentication()
+        .passwordEncoder(passwordEncoder())
+        .withUser("user").password(password)
+        .roles("USER");
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
+   
+

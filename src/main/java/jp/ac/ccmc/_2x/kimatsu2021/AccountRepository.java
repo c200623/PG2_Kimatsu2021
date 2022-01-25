f51313e53bbdf468bfa3e5ce89a6469cc2bdb5ff
c200623 <c200623@ccg.ac.jp>
2022-01-25 03:51:01 -0800

    MainController.javaのGet and post Map(メソッドを変更する

--- src/main/java/jp/ac/ccmc/_2x/kimatsu2021/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package jp.ac.ccmc._2x.kimatsu2021;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
